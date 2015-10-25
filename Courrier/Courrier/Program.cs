d0c6dbb5a105fd12fd06eea4d9440c2b42763b8a
Axel Lefaux <axel.lefaux@outlook.fr>
2015-10-25 13:56:11 +0100

    This is the startes project of Courrier

---
+++ Courrier/Courrier/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pqtmain
+{
+    class Courrier
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
